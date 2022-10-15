e57ed8e59a01bbd6e706ee4e2087deeab281a869
CarlosVelasquezAyan <carlos.velasquez.a@mail.pucv.cl>
2022-10-15 14:03:33 -0300

    First commit front Rick and Morty single spa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack:{
+    output: {
+      libraryTarget: 'system',
+    },
+  },
   transpileDependencies: true
 })
